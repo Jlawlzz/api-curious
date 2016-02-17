fe883baf3498e4519ff5b426fa3d2386737c3a2b
Jordan Lawler <jordan.lawler@colorado.edu>
2016-02-17 15:16:43 -0700

    Basic Soundcloud done, I FEEL GOOD I LOOK GOOD

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,7 +4,6 @@ class ServicesController < ApplicationController
     client = Soundcloud.new(:client_id => 'e6cec03e9db1f86a994857320fa6b7e3',
                         :client_secret => '82d05d53abcd4e4b44a4e1ddf5baed29',
                         :redirect_uri => 'http://localhost:3000/callback')
-
     redirect_to client.authorize_url()
   end
 
@@ -12,29 +11,33 @@ class ServicesController < ApplicationController
     client = Soundcloud.new(:client_id => 'e6cec03e9db1f86a994857320fa6b7e3',
                         :client_secret => '82d05d53abcd4e4b44a4e1ddf5baed29',
                         :redirect_uri => 'http://localhost:3000/callback')
-                        # raise params.inspect
     code = params[:code]
     session[:access_token] = client.exchange_token(:code => code)
-
     username = set_client.get('/me').username
-
-    @user = User.find_by(username: username)
-
-    if @user
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      @user = User.create(username: username)
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    end
+    user = User.find_by(username: username)
+    route_login(user)
   end
 
   def search
     client = set_client
-    @tracks = client.get('/tracks', q: params["search"]["search"], :license => 'cc-by')
+    results = client.get('/tracks', q: params["search"]["search"], :license => 'cc-by', limit: 50)
+    @tracks = results['collection']
+    @next = results['next_href']
     respond_to do |format|
         format.js
+    end
+  end
+
+  private
+
+  def route_login(user)
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     end
   end
 
